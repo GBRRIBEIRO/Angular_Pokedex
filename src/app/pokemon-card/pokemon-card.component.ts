4cdf6b306469d9d24105b3d98341ad44b3c04434
Gabriel Nunes Ribeiro <gabriel.ribeiro@programmers.com.br>
2022-06-07 00:19:48 -0300

    HTML and SCSS changes

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -6,5 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-card.component.scss']
 })
 export class PokemonCardComponent{
+  @Input()
+  pokemon : string;
+
+  @Input()
+  id : number;
+
+  takePokemonImage(){
+    const formatedNumber = this.leadingZeros(this.id) ;
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${formatedNumber}.png`;
+  }
+
+
+  leadingZeros(str:string | number, size = 3): string {
+    let s = String(str);
+
+    while (s.length < (size || 2 )){
+      s= '0' + s;
+      }
+
+    return s;
+  }
 
 }
